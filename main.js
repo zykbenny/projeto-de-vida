a4d592c509f33de26eea90d13489805c70718d27
benny <bianca.graczyk.lima@escola.pr.gov.br>
2026-05-21 13:57:02 +0000

    correção

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
@@ -11,5 +12,3 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
